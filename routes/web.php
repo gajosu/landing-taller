96768c630931eb88a85f4e3b4ff86c96b6a331f5
Gabriel Gonzalez <gajosu15@gmail.com>
2024-04-16 12:44:50 -0500

    add dewalt route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->redirectToRoute('dewalt');
 });
+
+Route::get('/dewalt', function () {
+    return view('welcome');
+})->name('dewalt');
 
 Route::get('/thanks', function () {
     return view('thanks');
