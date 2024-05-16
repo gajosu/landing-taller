d9926e5d400677a63b8b13ce1990abb1602a4a97
Gabriel Gonzalez <gajosu15@gmail.com>
2024-05-16 00:40:59 -0500

    add unique validations by event_name

--- app/Http/Controllers/InscriptionController.php
+++ app/Http/Controllers/InscriptionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Inscription;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class InscriptionController extends Controller
 {
@@ -14,14 +15,25 @@ class InscriptionController extends Controller
             'firstname' => 'required',
             'lastname' => 'required',
             'birthday' => 'required|date',
-            'cedula' => 'required|numeric|unique:inscriptions,cedula',
+            'cedula' => [
+                'required',
+                'numeric',
+                Rule::unique('inscriptions')->where(function ($query) use ($request) {
+                    return $query->where('event_name', $request->event_name);
+                }),
+            ],
             'phone' => 'required|numeric',
-            'email' => 'required|email|unique:inscriptions,email',
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('inscriptions')->where(function ($query) use ($request) {
+                    return $query->where('event_name', $request->event_name);
+                }),
+            ],
             'specialty' => ['required', 'in:' . implode(',', ['Eléctrico', 'Carpintero', 'Metalmecánico', 'Mecánico Automotriz', 'Línea Madera', 'Jardinería', 'General'])],
             'business_type' => ['required', 'in:' . implode(',', ['Ferretería', 'Constructora – Contratista', 'Industria', 'Independiente', 'Otros'])],
             'event_name' => ['required', 'in:' . implode(',', ['Dewalt', 'Black & Decker'])],
         ]);
-
         Inscription::create($request->all());
 
         return redirect()->route('thanks');
