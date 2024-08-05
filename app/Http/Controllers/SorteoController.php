c045380c624d56352e3ff56e2c89c7aec0266cc1
Gabriel Gonzalez <gajosu15@gmail.com>
2024-08-05 10:05:16 -0500

    fix

--- app/Http/Controllers/SorteoController.php
+++ app/Http/Controllers/SorteoController.php
@@ -33,7 +33,7 @@ class SorteoController extends Controller
 
         // put imagen with laravel storage
         $file = Storage::disk('real_public')->put('sorteo', $request->file('imagen'));
-        $url = Storage::url('sorteo/' . $file);
+        $url = Storage::url($file);
         $data = $request->except(['_token']);
         $data['imagen'] = $url;
         Sorteo::create($data);
