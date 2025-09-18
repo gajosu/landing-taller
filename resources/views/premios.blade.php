5bfe9a19dce6089b784297aa787871ddb1ed8e53
Gabriel González <gajosu15@gmail.com>
2025-09-18 20:16:00 +0000

    update file input label and accept attribute for invoice document

--- resources/views/premios.blade.php
+++ resources/views/premios.blade.php
@@ -138,9 +138,9 @@
                             </div>
 
                             <div class="col-span-1">
-                                <label for="imagen" class="block text-sm font-medium text-gray-700">Imagen de la Factura</label>
-                                <input type="file" name="imagen" id="imagen" required
-                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm text-black border-2 border-gray-300 rounded-md py-2 px-3">
+                                <label for="imagen" class="block text-sm font-medium text-gray-700">Documento de la factura (.pdf)</label>
+                                    <input type="file" name="imagen" id="imagen" required accept="application/pdf"
+                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm text-black border-2 border-gray-300 rounded-md py-2 px-3">
                                 <!-- Nota: El campo de archivo no puede conservar el valor anterior debido a restricciones de seguridad -->
                             </div>
 
